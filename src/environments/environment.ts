d434052dfbbe0bab20dde5bb92f35e1ccc5c0710
niagui <abdoniagui@gmail.com>
2020-10-04 12:50:24 +0100

    from local api to external api using heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/gestion-employes'
+  apiUrl: 'https://gestion-employes.herokuapp.com/gestion-employes'
 };
 
 /*
